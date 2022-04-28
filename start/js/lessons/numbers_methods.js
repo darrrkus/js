fbc07e3d18552a8f9ba9952b176cb61f21026874
darrrkus <darrrkus@gmail.com>
2022-04-28 16:32:46 +0300

    added numbers functions lesson

---
+++ start/js/lessons/numbers_methods.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const num = 135.2;
+console.log(Math.round(num));//135
+
+const widthStr = "12.5px";
+const widthInt = parseInt(widthStr);
+console.log(widthInt);//12
+const widthFloat = parseFloat(widthStr);
+console.log(widthFloat);//12.2
