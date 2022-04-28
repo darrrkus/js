a78d4981547df5a47020b3a22812be2248495628
darrrkus <darrrkus@gmail.com>
2022-04-28 12:49:21 +0300

    refactored main task, added some conditions

--- start/js/tasks/main_task.js
+++ start/js/tasks/main_task.js
@@ -1,6 +1,13 @@
 "use strict";
 
-let numberOfFilms = +prompt("How many movies have you seen?","");
+let numberOfFilms = +prompt("How many movies have you seen?", "");
+if (numberOfFilms < 10) {
+    alert("Too few movies!");
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+    alert("You are classic viewer!");
+} else {
+    alert("You are cinephil!");
+}
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,13 +16,18 @@ let personalMovieDB = {
     privat: false
 
 };
-let lastSeenTitle1 = prompt("Last seen movie #1?","");
-let lastSeenRate1 = prompt("Rate #1?","");
+for (let i = 0; i < 2; i++) {
+    let lastSeenTitle = prompt(`Last seen movie #${i + 1}?`, "");
+    if (!lastSeenTitle || lastSeenTitle.length > 50) {
+        i--;
+        continue;
+    } else {
+        let lastSeenRate = prompt(`Rate movie #${i + 1}?`, "");
+        personalMovieDB.movies[lastSeenTitle] = lastSeenRate;
+    }
+}
 
-let lastSeenTitle2 = prompt("Last seen movie #2?","");
-let lastSeenRate2 = prompt("Rate #2?","");
 
-personalMovieDB.movies[lastSeenTitle1] = lastSeenRate1;
-personalMovieDB.movies[lastSeenTitle2] = lastSeenRate2;
+
 
 console.log(personalMovieDB);
