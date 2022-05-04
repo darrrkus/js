9fa9dd37ee30575ad3e53deaf695f2a6ab61206f
darrrkus <darrrkus@gmail.com>
2022-05-04 17:15:36 +0300

    added test task about objects, destruction etc

---
+++ start/js/tasks/objects.js
@@ -0,0 +1,41 @@
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(plan){
+        const {age} = plan;
+        const {languages} = plan.skills;
+        let resStr =`Мне ${age} и я владею языками: `;
+        languages.forEach(language => {
+            resStr += language.toUpperCase();
+            resStr+= ' ';
+        });
+        return resStr;
+    }
+};
+
+function showExperience(plan) {
+    const {exp} = plan.skills;
+    return exp;
+}
+
+function showProgrammingLangs(plan) {
+    let str = '';
+    const {programmingLangs} = plan.skills;
+    for (let key in programmingLangs) {
+        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
+    }
+
+    return str;
+}
+
+console.log(showExperience(personalPlanPeter));
+console.log(showProgrammingLangs(personalPlanPeter));
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
